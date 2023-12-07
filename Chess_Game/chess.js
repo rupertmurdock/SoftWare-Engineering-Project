349794f0b3af2f6f87a639d8cc659e21acd536b3
Tarmu Hu <hutarmu@gmail.com>
2023-12-06 19:18:03 -0800

    010

--- Chess_Game/chess.js
+++ Chess_Game/chess.js
@@ -1,20 +1,24 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
-import { getDatabase, ref, set, update, child, get } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
-import { getAuth, } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBDZFqhkECAYqdxUDEUF96Rb9OCUh6Jl64",
-  authDomain: "game-1-website.firebaseapp.com",
-  databaseURL: "https://game-1-website-default-rtdb.firebaseio.com",
-  projectId: "game-1-website",
-  storageBucket: "game-1-website.appspot.com",
-  messagingSenderId: "483277766163",
-  appId: "1:483277766163:web:55358a49073017e46609e6"
-};
-            
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
-const auth = getAuth();
+        import { getDatabase, set, ref, update, child, get } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
+        import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+        // TODO: Add SDKs for Firebase products that you want to use
+        // https://firebase.google.com/docs/web/setup#available-libraries
+      
+        // Your web app's Firebase configuration
+        const firebaseConfig = {
+          apiKey: "AIzaSyBDZFqhkECAYqdxUDEUF96Rb9OCUh6Jl64",
+          authDomain: "game-1-website.firebaseapp.com",
+          databaseURL: "https://game-1-website-default-rtdb.firebaseio.com",
+          projectId: "game-1-website",
+          storageBucket: "game-1-website.appspot.com",
+          messagingSenderId: "483277766163",
+          appId: "1:483277766163:web:55358a49073017e46609e6"
+        };
+      
+        // Initialize Firebase
+        const app = initializeApp(firebaseConfig);
+        const database = getDatabase(app);
+        const auth = getAuth();
 
 function chessWin(){
   const user = auth.currentUser;
