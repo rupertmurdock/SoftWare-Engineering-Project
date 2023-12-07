8f33bf9b45140700fd090e8eeeed3730c970afce
Tarmu Hu <hutarmu@gmail.com>
2023-12-06 17:50:52 -0800

    a

--- Chess_Game/chess.js
+++ Chess_Game/chess.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
-import { getDatabase, ref, update, child, get } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
+import { getDatabase, ref, set, update, child, get } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
 import { getAuth, } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
 
 const firebaseConfig = {
